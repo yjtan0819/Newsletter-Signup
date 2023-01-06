c3398f241b45b9d3fdb9530394bbdaa5e0e456a2
yjtan0819 <tan.yijia0819@gmail.com>
2023-01-05 23:03:05 -0500

    Moved pictures to be used as static

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
 const app = express();
 const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
 
-app.listen(port, ()=>{
-    console.log(`Server running on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
