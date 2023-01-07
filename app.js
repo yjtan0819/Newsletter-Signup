db61b3254c06cd6d41680474c7238404979fbf17
yjtan0819 <tan.yijia0819@gmail.com>
2023-01-06 21:16:23 -0500

    Add failure and success pages

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 const app = express();
 const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,9 +15,46 @@ app.post("/", (req, res) => {
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
     const email = req.body.email;
-    console.log(firstName, lastName, email);
+    var data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+    var jsonData = JSON.stringify(data);
+    const url = "https://us11.api.mailchimp.com/3.0/lists/517aff78d8";
+
+    const options = {
+        method: "POST",
+        auth: "yjtan0819:98c0760c999197e2c5b2b5a26cfef403-us11"
+    }
+
+    const request = https.request(url, options, function(response) {
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data", function(data) {
+            console.log(JSON.parse(data));
+        });
+    });
+    
+    request.write(jsonData);
+    request.end();
 });
 
+app.post("/failure", (req, res) => {
+    res.redirect("/");
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
